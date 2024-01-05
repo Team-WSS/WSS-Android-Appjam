0fe67b91074df4b015d5df8ba865cb926437f66a
librarywon <librarywon@naver.com>
2024-01-06 03:01:00 +0900

    build: fragment 전환시 record 오타 수정

--- app/src/main/java/com/teamwss/websoso/ui/main/MainActivity.kt
+++ app/src/main/java/com/teamwss/websoso/ui/main/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
             val selectedFragment = when (item.itemId) {
                 R.id.menu_home -> HomeFragment.newInstance()
                 R.id.menu_library -> LibraryFragment.newInstance()
-                R.id.menu_recode -> RecordFragment.newInstance()
+                R.id.menu_record -> RecordFragment.newInstance()
                 R.id.menu_my_page -> MyPageFragment.newInstance()
                 else -> null
             }
