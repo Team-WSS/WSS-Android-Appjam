3c981bc12f2f9442f7e43845dff1f4df4f473074
junseo511 <prime54@g.skku.edu>
2024-01-06 02:17:27 +0900

    feat: PostNovelActivity UI 구현

--- app/src/main/java/com/teamwss/websoso/ui/postNovel/PostNovelActivity.kt
+++ app/src/main/java/com/teamwss/websoso/ui/postNovel/PostNovelActivity.kt
@@ -1,12 +1,31 @@
 package com.teamwss.websoso.ui.postNovel
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
-import com.teamwss.websoso.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.NestedScrollView
+import com.google.android.material.appbar.AppBarLayout
+import com.teamwss.websoso.databinding.ActivityPostNovelBinding
 
 class PostNovelActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPostNovelBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_post_novel)
+        binding = ActivityPostNovelBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupAppBar()
+    }
+
+    private fun setupAppBar() {
+        val nestedScrollView: NestedScrollView = binding.svPost
+        val appBarLayout: AppBarLayout = binding.alPostAppBar
+
+        nestedScrollView.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { _, _, scrollY, _, _ ->
+            val scrollRatio = scrollY.toFloat() / appBarLayout.height
+            val colorAlpha = 255.coerceAtMost((scrollRatio * 255).toInt())
+
+            appBarLayout.setBackgroundColor(Color.argb(colorAlpha, 255, 255, 255))
+        })
     }
 }
