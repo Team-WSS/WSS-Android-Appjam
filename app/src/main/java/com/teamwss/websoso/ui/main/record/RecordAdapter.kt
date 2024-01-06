ce7e40f616ac8a7a9f8a509cb6a3a8062c3a3c60
yeonjeen <jeendev2019@gmail.com>
2024-01-06 21:13:12 +0900

    feat: record 어댑터 생성

---
+++ app/src/main/java/com/teamwss/websoso/ui/main/record/RecordAdapter.kt
@@ -0,0 +1,28 @@
+package com.teamwss.websoso.ui.main.record
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.teamwss.websoso.data.Memo
+import com.teamwss.websoso.databinding.ItemMemoBinding
+
+class RecordAdapter(context: Context) : RecyclerView.Adapter<RecordViewHolder>() {
+    private val inflater by lazy { LayoutInflater.from(context) }
+    private var memoList: List<Memo> = emptyList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecordViewHolder {
+        val binding = ItemMemoBinding.inflate(inflater, parent, false)
+        return RecordViewHolder(binding)
+    }
+
+    override fun getItemCount() = memoList.size
+    fun setFriendList(memoList: List<Memo>) {
+        this.memoList = memoList.toList()
+        notifyDataSetChanged()
+    }
+    override fun onBindViewHolder(holder: RecordViewHolder, position: Int) {
+        holder.onBind(memoList[position])
+    }
+
+}
