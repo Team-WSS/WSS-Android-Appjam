b60a60867f07c5aba7fe7761b7c35bf4208c0002
m6z1 <sonmyungj1zz@gmail.com>
2024-01-08 01:37:38 +0900

    feat: 배너의 썸네일 이미지 모서리 둥글게 적용

--- app/src/main/java/com/teamwss/websoso/ui/novelDetail/NovelDetailActivity.kt
+++ app/src/main/java/com/teamwss/websoso/ui/novelDetail/NovelDetailActivity.kt
@@ -1,12 +1,17 @@
 package com.teamwss.websoso.ui.novelDetail
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.teamwss.websoso.R
+import androidx.appcompat.app.AppCompatActivity
+import com.teamwss.websoso.databinding.ActivityNovelDetailBinding
 
 class NovelDetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNovelDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_novel_detail)
+        binding = ActivityNovelDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ivNovelDetailThumbnail.clipToOutline = true
     }
 }
