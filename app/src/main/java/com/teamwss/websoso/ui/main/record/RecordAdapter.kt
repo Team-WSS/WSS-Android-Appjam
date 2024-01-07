3c92624ea6bc70043c2a3d5ce561f16273ce2d74
yeonjeen <jeendev2019@gmail.com>
2024-01-07 20:54:01 +0900

    refactor: viewholder와 adapter 분리

--- app/src/main/java/com/teamwss/websoso/ui/main/record/RecordAdapter.kt
+++ app/src/main/java/com/teamwss/websoso/ui/main/record/RecordAdapter.kt
@@ -1,18 +1,14 @@
 package com.teamwss.websoso.ui.main.record
 
-import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.teamwss.websoso.data.Memo
-import com.teamwss.websoso.databinding.ItemMemoBinding
 
 class RecordAdapter : RecyclerView.Adapter<RecordViewHolder>() {
     private var memoList: List<Memo> = emptyList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecordViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val binding = ItemMemoBinding.inflate(inflater, parent, false)
-        return RecordViewHolder(binding)
+        return RecordViewHolder.create(parent)
     }
 
     override fun getItemCount() = memoList.size
--- app/src/main/java/com/teamwss/websoso/ui/main/record/RecordViewHolder.kt
+++ app/src/main/java/com/teamwss/websoso/ui/main/record/RecordViewHolder.kt
@@ -1,11 +1,21 @@
 package com.teamwss.websoso.ui.main.record
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.teamwss.websoso.data.Memo
 import com.teamwss.websoso.databinding.ItemMemoBinding
 
 class RecordViewHolder(private val binding: ItemMemoBinding) :
     RecyclerView.ViewHolder(binding.root) {
+
+    companion object {
+        fun create(parent: ViewGroup): RecordViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val binding = ItemMemoBinding.inflate(inflater, parent, false)
+            return RecordViewHolder(binding)
+        }
+    }
 
     fun onBind(memo: Memo) {
         binding.tvMemoDate.text = memo.novelDate
