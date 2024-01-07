59c0f2325fa8643e9f0baf304e42def12c239252
yeonjeen <jeendev2019@gmail.com>
2024-01-07 20:01:49 +0900

    refactor: LayoutInflater.from(parent.context) 사용

--- app/src/main/java/com/teamwss/websoso/ui/main/record/RecordAdapter.kt
+++ app/src/main/java/com/teamwss/websoso/ui/main/record/RecordAdapter.kt
@@ -1,17 +1,16 @@
 package com.teamwss.websoso.ui.main.record
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.teamwss.websoso.data.Memo
 import com.teamwss.websoso.databinding.ItemMemoBinding
 
-class RecordAdapter(context: Context) : RecyclerView.Adapter<RecordViewHolder>() {
-    private val inflater by lazy { LayoutInflater.from(context) }
+class RecordAdapter : RecyclerView.Adapter<RecordViewHolder>() {
     private var memoList: List<Memo> = emptyList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecordViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
         val binding = ItemMemoBinding.inflate(inflater, parent, false)
         return RecordViewHolder(binding)
     }
