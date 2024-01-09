c3bb1e539866f6dff7cf014c37d1048d29a6d7a9
m6z1 <sonmyungj1zz@gmail.com>
2024-01-09 22:56:21 +0900

    feat: 메모 작성 페이지 뷰 구현(이미지 제외)

--- app/src/main/java/com/teamwss/websoso/ui/memoWrite/MemoWriteActivity.kt
+++ app/src/main/java/com/teamwss/websoso/ui/memoWrite/MemoWriteActivity.kt
@@ -1,12 +1,22 @@
 package com.teamwss.websoso.ui.memoWrite
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import com.teamwss.websoso.R
 
 class MemoWriteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_memo_write)
+
+        setTranslucentOnStatusBar()
+    }
+
+    private fun setTranslucentOnStatusBar() {
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
     }
 }
