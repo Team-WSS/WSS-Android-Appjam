aa3bc6c71d53e49f7fa99eda640d93c5ca217156
m6z1 <sonmyungj1zz@gmail.com>
2024-01-09 22:16:37 +0900

    feat: 메모 작성 페이지로 네비게이션 구현

--- app/src/main/java/com/teamwss/websoso/ui/novelDetail/fragment/NovelMemoFragment.kt
+++ app/src/main/java/com/teamwss/websoso/ui/novelDetail/fragment/NovelMemoFragment.kt
@@ -1,11 +1,13 @@
 package com.teamwss.websoso.ui.novelDetail.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.teamwss.websoso.databinding.FragmentNovelMemoBinding
+import com.teamwss.websoso.ui.memoWrite.MemoWriteActivity
 
 class NovelMemoFragment : Fragment() {
     private var _binding: FragmentNovelMemoBinding? = null
@@ -13,11 +15,33 @@ class NovelMemoFragment : Fragment() {
         get() = requireNotNull(_binding)
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentNovelMemoBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        clickAddingMemoFab()
+        clickAddingMemoBox()
+    }
+
+    private fun clickAddingMemoFab() {
+        binding.btnNovelMemoAddMemo.setOnClickListener {
+            navigateMemoWrite()
+        }
+    }
+
+    private fun clickAddingMemoBox() {
+        binding.clNovelMemoNavigateNewMemo.setOnClickListener {
+            navigateMemoWrite()
+        }
+    }
+
+    private fun navigateMemoWrite() {
+        val intent = Intent(activity, MemoWriteActivity::class.java)
+        startActivity(intent)
     }
 }
