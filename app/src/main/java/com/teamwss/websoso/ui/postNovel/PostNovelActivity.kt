2298fa262601ba73b4c7ad46bf411e12cd001090
junseo511 <prime54@g.skku.edu>
2024-01-10 10:20:17 +0900

    feat: PostNovelViewModel 생성

--- app/src/main/java/com/teamwss/websoso/ui/postNovel/PostNovelActivity.kt
+++ app/src/main/java/com/teamwss/websoso/ui/postNovel/PostNovelActivity.kt
@@ -13,6 +13,10 @@ import kotlin.math.pow
 
 class PostNovelActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPostNovelBinding
+    private val postNovelViewModel: PostNovelViewModel by lazy {
+        PostNovelViewModel()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPostNovelBinding.inflate(layoutInflater)
@@ -23,7 +27,7 @@ class PostNovelActivity : AppCompatActivity() {
         setupDateToggle()
         setupReadStatusChip()
         showDatePickerDialog()
-        initDummyNovelInfo()
+        initNovelInfo()
     }
 
     private val setupNavigateLeftDialog: PostNavigateLeftDialog by lazy {
@@ -145,29 +149,48 @@ class PostNovelActivity : AppCompatActivity() {
         }
     }
 
-    private fun initDummyNovelInfo() {
+    private fun initNovelInfo() {
+        val dummyData = postNovelViewModel.getUserNovelInfo()
         with(binding) {
-            tvPostNovelTitle.text = "노 게임 노 라이프"
-            tvPostTitle.text = "노 게임 노 라이프"
-            tvPostNovelAuthor.text = "카미야 유우"
-            ivPostCover.load("https://i.namu.wiki/i/j1S3TlFyve1UjbCnzF_g6qEgFnMi8usZ_DLCn8lP91FwgpPgwkv_GNCD2fmu5uEPgPU5CSdzDF5qwe_8Ains2UzdgGgI-bzT95MQeBrceU9E7Hr26fWBFREMLDGiZm01VtAXHgXRO9kviGz3sYwQ-w.webp") {
+            tvPostNovelTitle.text = dummyData.userNovelTitle
+            tvPostTitle.text = tvPostNovelTitle.text
+            tvPostNovelAuthor.text = dummyData.userNovelAuthor
+            tvPostNovelDetail.text = dummyData.userNovelDescription
+            tvPostNovelGenre.text = dummyData.userNovelGenre
+
+            rbPostRating.rating = dummyData.userNovelRating
+            tvPostReadDateStart.text = dummyData.readStartDate
+            tvPostReadDateEnd.text = dummyData.readEndDate
+
+            val coverImg = dummyData.userNovelImg
+            val loadingImg = R.drawable.img_loading_thumbnail
+            ivPostCover.load(coverImg) {
                 crossfade(true)
-                placeholder(R.drawable.img_loading_thumbnail)
-                error(R.drawable.img_loading_thumbnail)
+                placeholder(loadingImg)
+                error(loadingImg)
                 transformations(RoundedCornersTransformation(30))
             }
-            ivPostCoverBackground.load("https://i.namu.wiki/i/j1S3TlFyve1UjbCnzF_g6qEgFnMi8usZ_DLCn8lP91FwgpPgwkv_GNCD2fmu5uEPgPU5CSdzDF5qwe_8Ains2UzdgGgI-bzT95MQeBrceU9E7Hr26fWBFREMLDGiZm01VtAXHgXRO9kviGz3sYwQ-w.webp") {
+            ivPostCoverBackground.load(coverImg) {
                 crossfade(true)
-                placeholder(R.drawable.img_loading_thumbnail)
-                error(R.drawable.img_loading_thumbnail)
+                placeholder(loadingImg)
+                error(loadingImg)
                 transformations(BlurTransformation(this@PostNovelActivity, 25))
             }
-            rbPostRating.rating = 5.0f
-            tvPostReadDateStart.text = "2023-12-26"
-            tvPostReadDateEnd.text = "2023-12-26"
-            tvPostNovelDetail.text =
-                "백수에 골방지기지만 인터넷에서는 도시전설이라는 이야기마저 떠도는 천재 게이머 남매, 소라(空)와 시로(白). 둘이 합쳐 하나인 『　　』(공백)인 남매는 세상을 「쓰레기 게임」이라 부르며 지내던 어느 날, 『신』을 자칭하는 소년에게 이끌려 이세계로 소환된다. 그곳은 신에 의해 전쟁이 금지되었으며, 『모든 것』── 「국경선마저도 게임으로 결판이 나는」 세계였다.\n\n다른 종족들에게 연패를 거듭해 마지막 도시 하나만을 남겨둔 인류. 소라와 시로 폐인남매가 이세계에서는 ‘인류의 구세주’가 될 수 있을까?"
-            tvPostNovelGenre.text = "라이트노벨"
+
+            when (dummyData.userNovelReadStatus) {
+                getString(R.string.post_read_status_read) -> {
+                    cReadStatusRead.isChecked = true
+                }
+                getString(R.string.post_read_status_reading) -> {
+                    cReadStatusReading.isChecked = true
+                }
+                getString(R.string.post_read_status_stop) -> {
+                    cReadStatusStop.isChecked = true
+                }
+                else -> {
+                    cReadStatusWant.isChecked = true
+                }
+            }
         }
     }
 }
