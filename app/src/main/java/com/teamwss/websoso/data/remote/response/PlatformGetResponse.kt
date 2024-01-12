c8499202fcad5b69c0b034e1722202d88ae74316
junseo511 <prime54@g.skku.edu>
2024-01-13 06:15:23 +0900

    fix: PlatformGetResponse 명세서에 맞게 수정

--- app/src/main/java/com/teamwss/websoso/data/remote/response/PlatformGetResponse.kt
+++ app/src/main/java/com/teamwss/websoso/data/remote/response/PlatformGetResponse.kt
@@ -5,8 +5,6 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class PlatformGetResponse(
-    @SerialName("platformId")
-    val platformId: Int = 0,
     @SerialName("platformName")
     val platformName: String = "",
     @SerialName("platformUrl")
