7355cba4f0f77924064efabbc83d173c606b7568
yeonjeen <jeendev2019@gmail.com>
2024-01-14 00:43:28 +0900

    feat: 아바타 바텀다이얼로그 구현

---
+++ app/src/main/java/com/teamwss/websoso/ui/main/myPage/AvatarDialogFragment.kt
@@ -0,0 +1,51 @@
+package com.teamwss.websoso.ui.main.myPage
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.teamwss.websoso.R
+import com.teamwss.websoso.databinding.DialogAvatarBinding
+import com.teamwss.websoso.ui.main.myPage.model.Avatar
+
+class AvatarDialogFragment : DialogFragment() {
+    private lateinit var binding: DialogAvatarBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DialogAvatarBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupDialogUI()
+    }
+
+    private fun setupDialogUI() {
+        dialog?.window?.apply {
+            setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+            setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            attributes?.windowAnimations = R.style.BottomSheetDialogAnimation
+            setGravity(Gravity.BOTTOM)
+        }
+    }
+
+    companion object {
+        const val TAG = "AvatarDialogFragment"
+
+        fun newInstance(avatar: Avatar): AvatarDialogFragment {
+            return AvatarDialogFragment()
+        }
+    }
+
+}
+
