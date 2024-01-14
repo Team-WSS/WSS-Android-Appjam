bdafd71922bd00aa16f919b7753ac05722d5833c
librarywon <librarywon@naver.com>
2024-01-15 07:08:35 +0900

    feat: novel 개수에 따른 empty뷰 visibility 속성 변화 로직 추가

--- app/src/main/java/com/teamwss/websoso/ui/main/library/adapter/LibraryViewPagerViewHolder.kt
+++ app/src/main/java/com/teamwss/websoso/ui/main/library/adapter/LibraryViewPagerViewHolder.kt
@@ -1,15 +1,21 @@
 package com.teamwss.websoso.ui.main.library.adapter
 
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.teamwss.websoso.data.model.LibraryUserNovelEntity
 import com.teamwss.websoso.databinding.ItemLibraryViewPagerBinding
 
 class LibraryViewPagerViewHolder(
     private val binding: ItemLibraryViewPagerBinding,
+    private val onItemClick: (Long) -> Unit
 ) : RecyclerView.ViewHolder(binding.root) {
-    private val libraryItemAdapter = LibraryItemAdapter()
+    private val libraryItemAdapter = LibraryItemAdapter(onItemClick)
 
     fun bind(novels: List<LibraryUserNovelEntity>) {
+        binding.clLibraryEmpty.apply {
+            visibility = if (novels.isEmpty()) View.VISIBLE else View.GONE
+        }
+
         libraryItemAdapter.submitList(novels)
         binding.rvLibraryViewPager.adapter = libraryItemAdapter
     }
