303d57d5681fdea71274a0917b70cf44e061dd59
librarywon <librarywon@naver.com>
2024-01-15 00:00:12 +0900

    build: data class 이름 변경

--- app/src/main/java/com/teamwss/websoso/data/model/LibraryUserNovelEntity.kt
+++ app/src/main/java/com/teamwss/websoso/data/model/LibraryUserNovelEntity.kt
@@ -1,7 +1,7 @@
 package com.teamwss.websoso.data.model
 
 
-data class LibraryUserNovel(
+data class LibraryUserNovelEntity(
     val userNovelId: Long,
     val userNovelTitle: String,
     val userNovelCover: String,
