3a87d61330f4f51260020f57f30099bb9ff9b4ee
junseo511 <prime54@g.skku.edu>
2024-01-15 00:03:19 +0900

    build: api 서비스별 분류

--- app/src/main/java/com/teamwss/websoso/data/NetworkModule.kt
+++ app/src/main/java/com/teamwss/websoso/data/NetworkModule.kt
@@ -3,7 +3,11 @@ package com.teamwss.websoso.data
 import android.util.Log
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.teamwss.websoso.BuildConfig
-import com.teamwss.websoso.data.remote.api.WssService
+import com.teamwss.websoso.data.remote.service.AuthService
+import com.teamwss.websoso.data.remote.service.AvatarService
+import com.teamwss.websoso.data.remote.service.MemoService
+import com.teamwss.websoso.data.remote.service.NovelService
+import com.teamwss.websoso.data.remote.service.UserNovelService
 import kotlinx.serialization.json.Json
 import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
@@ -33,12 +37,16 @@ object NetworkModule {
     val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .client(okHttpClient)
-        .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+        .addConverterFactory(json.asConverterFactory(CONTENT_TYPE.toMediaType()))
         .build()
 
     inline fun <reified T> create(): T = retrofit.create<T>(T::class.java)
 }
 
 object ServicePool {
-    val wssService: WssService by lazy { NetworkModule.create() }
+    val authService: AuthService by lazy { NetworkModule.create() }
+    val userNovelService: UserNovelService by lazy { NetworkModule.create() }
+    val novelService: NovelService by lazy { NetworkModule.create() }
+    val avatarService: AvatarService by lazy { NetworkModule.create() }
+    val memoService: MemoService by lazy { NetworkModule.create() }
 }
--- app/src/main/java/com/teamwss/websoso/data/remote/service/NovelService.kt
+++ app/src/main/java/com/teamwss/websoso/data/remote/service/NovelService.kt
@@ -1,35 +1,30 @@
-package com.teamwss.websoso.data.remote.api
+package com.teamwss.websoso.data.remote.service
 
 import com.teamwss.websoso.data.remote.request.PostNovelRequest
 import com.teamwss.websoso.data.remote.response.EditNovelResponse
 import com.teamwss.websoso.data.remote.response.PostNovelResponse
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Path
 
-interface WssService {
+interface NovelService {
 
-    @Headers("Authorization")
     @GET("/novels/{novelId}")
     suspend fun getPostNovelInfo(
         @Path("novelId") novelId: Long
     ): PostNovelResponse
 
-    @Headers("Authorization")
     @GET("/novels/{novelId}")
     suspend fun getEditNovelInfo(
         @Path("novelId") novelId: Long
     ): EditNovelResponse
 
-    @Headers("Authorization")
     @POST("/user-novels/{novelId}")
     suspend fun postPostNovelInfo(
         @Path("novelId") novelId: Long
     ): PostNovelRequest
 
-    @Headers("Authorization")
     @PATCH("/user-novels/{userNovelId}")
     suspend fun editPostNovelInfo(
         @Path("userNovelId") userNovelId: Long
