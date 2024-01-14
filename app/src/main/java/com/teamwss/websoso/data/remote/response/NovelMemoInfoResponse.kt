8a74c1d20834a5ebe42beb8201683558fca2c3d2
Son Myeongji <sonmyungj1zz@gmail.com>
2024-01-14 16:41:16 +0900

    build: DTO 타입 변경

--- app/src/main/java/com/teamwss/websoso/data/remote/response/NovelMemoInfoResponse.kt
+++ app/src/main/java/com/teamwss/websoso/data/remote/response/NovelMemoInfoResponse.kt
@@ -4,7 +4,7 @@ data class NovelMemoInfoResponse(
     val memos: List<NovelMemoResponse>,
     val platforms: List<PlatformGetResponse>,
     val userNovelGenre: String,
-    val userNovelRating: Double,
+    val userNovelRating: Float,
     val userNovelReadEndDate: String,
     val userNovelReadStartDate: String,
     val userNovelReadStatus: String,
