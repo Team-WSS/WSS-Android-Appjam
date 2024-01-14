497ff1fe0cc7d434676896fb5644377b4bcda856
librarywon <librarywon@naver.com>
2024-01-15 07:07:54 +0900

    feat: UserNovelsRepository 파라미터 수정

--- app/src/main/java/com/teamwss/websoso/data/repository/UserNovelsRepository.kt
+++ app/src/main/java/com/teamwss/websoso/data/repository/UserNovelsRepository.kt
@@ -1,17 +1,17 @@
 package com.teamwss.websoso.data.repository
 
-import com.teamwss.websoso.data.remote.request.UserNovelsLibraryRequest
 import com.teamwss.websoso.data.remote.service.UserNovelService
 
 class UserNovelsRepository(
     private val userNovelService: UserNovelService
 ) {
 
-    suspend fun getNovels(request: UserNovelsLibraryRequest) = userNovelService.getNovels(
-        readStatus = request.readStatus,
-        lastUserNovelId = request.lastUserNovelId,
-        size = request.size,
-        sortType = request.sortType.toString()
-    )
+    suspend fun getNovels(readState: String, lastUserNovelId: Long, size: Int, sortType: String) =
+        userNovelService.getNovels(
+            readStatus = readState,
+            lastUserNovelId = lastUserNovelId,
+            size = size,
+            sortType = sortType
+        )
 
 }
