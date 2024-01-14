ec6ac77dbf8b089ca4652bd5abc7e864229b99d4
librarywon <librarywon@naver.com>
2024-01-15 06:36:23 +0900

    feat: UserNovelService 추가 기존 서비스 삭제

--- app/src/main/java/com/teamwss/websoso/data/remote/service/UserNovelService.kt
+++ app/src/main/java/com/teamwss/websoso/data/remote/service/UserNovelService.kt
@@ -1,4 +1,17 @@
 package com.teamwss.websoso.data.remote.service
 
+import com.teamwss.websoso.data.remote.response.UserNovelsResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface UserNovelService {
+
+    @GET("user-novels")
+    suspend fun getNovels(
+        @Query("readStatus") readStatus: String,
+        @Query("lastUserNovelId") lastUserNovelId: Long,
+        @Query("size") size: Int,
+        @Query("sortType") sortType: String
+    ): UserNovelsResponse
+
 }
