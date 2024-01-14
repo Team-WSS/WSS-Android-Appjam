11b6c120ca86e0ce658766223e5f6d2b5aed0e5c
yeonjeen <jeendev2019@gmail.com>
2024-01-15 03:07:02 +0900

    refactor: 어댑터 뷰홀더 수정

--- app/src/main/java/com/teamwss/websoso/ui/main/myPage/MyPageAdapter.kt
+++ app/src/main/java/com/teamwss/websoso/ui/main/myPage/MyPageAdapter.kt
@@ -1,15 +1,19 @@
 package com.teamwss.websoso.ui.main.myPage
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.teamwss.websoso.databinding.ItemAvatarBinding
 import com.teamwss.websoso.ui.main.myPage.model.Avatar
 
 class MyPageAdapter : RecyclerView.Adapter<MyPageViewHolder>() {
-    internal var avatarItems: List<Avatar> = emptyList()
-    internal var onItemClickListener: ((Avatar) -> Unit)? = null
+    var avatarItems: List<Avatar> = emptyList()
+    var onItemClickListener: ((Avatar) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyPageViewHolder {
-        return MyPageViewHolder.create(parent, adapter = MyPageAdapter())
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemAvatarBinding.inflate(inflater, parent, false)
+        return MyPageViewHolder(binding, adapter = MyPageAdapter())
     }
 
     override fun onBindViewHolder(holder: MyPageViewHolder, position: Int) {
