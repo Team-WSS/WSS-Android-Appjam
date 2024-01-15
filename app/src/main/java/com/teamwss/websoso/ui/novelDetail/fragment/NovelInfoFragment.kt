6d0a8c09316f7799ebdd61fb210a053d7c406432
Son Myeongji <sonmyungj1zz@gmail.com>
2024-01-16 03:50:44 +0900

    build: 코드 포맷팅

--- app/src/main/java/com/teamwss/websoso/ui/novelDetail/fragment/NovelInfoFragment.kt
+++ app/src/main/java/com/teamwss/websoso/ui/novelDetail/fragment/NovelInfoFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import com.teamwss.websoso.databinding.FragmentNovelInfoBinding
 import com.teamwss.websoso.ui.novelDetail.NovelDetailViewModel
 
