56e1477039595022f186d5b7f2852fa230217486
junseo511 <prime54@g.skku.edu>
2024-01-15 18:09:09 +0900

    refactor: 디자이너 요구에 따른 블러 정도 수정

--- app/src/main/java/com/teamwss/websoso/util/BindingAdpater.kt
+++ app/src/main/java/com/teamwss/websoso/util/BindingAdpater.kt
@@ -13,9 +13,9 @@ fun loadCoverImageRounded30(view: ImageView, imageUrl: String?) {
     loadCustomImage(view, imageUrl, RoundedCornersTransformation(30F))
 }
 
-@BindingAdapter("loadCoverImageBlurred25")
-fun loadCoverImageBlurred25(view: ImageView, imageUrl: String?) {
-    loadCustomImage(view, imageUrl, BlurTransformation(view.context, 25))
+@BindingAdapter("loadCoverImageBlurred5")
+fun loadCoverImageBlurred5(view: ImageView, imageUrl: String?) {
+    loadCustomImage(view, imageUrl, BlurTransformation(view.context, 5))
 }
 
 private fun loadCustomImage(view: ImageView, imageUrl: String?, transformation: Transformation) {
