bc53932808603aa2057aa8c1fc82a971b027de35
yeonjeen <jeendev2019@gmail.com>
2024-01-16 04:34:30 +0900

    refactor: Record 함수 순서 변경

--- app/src/main/java/com/teamwss/websoso/ui/main/record/RecordViewHolder.kt
+++ app/src/main/java/com/teamwss/websoso/ui/main/record/RecordViewHolder.kt
@@ -9,19 +9,19 @@ import com.teamwss.websoso.ui.main.record.model.Memo
 class RecordViewHolder(private val binding: ItemMemoBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
-    companion object {
-        fun create(parent: ViewGroup): RecordViewHolder {
-            val inflater = LayoutInflater.from(parent.context)
-            val binding = ItemMemoBinding.inflate(inflater, parent, false)
-            return RecordViewHolder(binding)
-        }
-    }
-
     fun onBind(memo: Memo) {
         with(binding) {
             tvMemoDate.text = memo.novelDate
             tvMemoTitle.text = memo.novelTitle
             tvMemoContent.text = memo.novelContent
+        }
+    }
+
+    companion object {
+        fun create(parent: ViewGroup): RecordViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val binding = ItemMemoBinding.inflate(inflater, parent, false)
+            return RecordViewHolder(binding)
         }
     }
 }
