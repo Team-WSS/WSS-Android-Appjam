54dc8837b58fc2907e5629de57cee3cfd30c74c9
librarywon <librarywon@naver.com>
2024-01-17 01:46:16 +0900

    feat: 로티 이미지 확장함수 추가

---
+++ app/src/main/java/com/teamwss/websoso/util/LottieImageUtil.kt
@@ -0,0 +1,8 @@
+package com.teamwss.websoso.util
+
+import com.teamwss.websoso.R
+
+fun Int.toLottieImage() = when (this) {
+    1 -> R.raw.lottie_home_regressor
+    else -> R.raw.lottie_home_regressor
+}
