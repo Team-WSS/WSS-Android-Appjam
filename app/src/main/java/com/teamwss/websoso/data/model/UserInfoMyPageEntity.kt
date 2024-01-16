39849267402be6c13712c7f49da36ed83126efff
librarywon <librarywon@naver.com>
2024-01-17 04:46:44 +0900

    feat: UserInfoMyPageEntity생성

---
+++ app/src/main/java/com/teamwss/websoso/data/model/UserInfoMyPageEntity.kt
@@ -0,0 +1,15 @@
+package com.teamwss.websoso.data.model
+
+import com.teamwss.websoso.data.remote.response.UserAvatarResponse
+
+data class UserInfoMyPageEntity(
+    val memoCount: Long,
+    val representativeAvatarGenreBadge: String,
+    val representativeAvatarId: Long,
+    val representativeAvatarLineContent: String,
+    val representativeAvatarTag: String,
+    val representativeAvatarImg: String,
+    val userAvatars: List<UserAvatarResponse>,
+    val userNickName: String,
+    val userNovelCount: Long,
+)
