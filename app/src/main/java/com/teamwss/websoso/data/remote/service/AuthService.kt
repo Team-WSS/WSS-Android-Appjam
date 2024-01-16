963855fdc292e26dd11aaaaaaa35827358731539
librarywon <librarywon@naver.com>
2024-01-17 03:52:21 +0900

    feat: LoginResponse 수정

--- app/src/main/java/com/teamwss/websoso/data/remote/service/AuthService.kt
+++ app/src/main/java/com/teamwss/websoso/data/remote/service/AuthService.kt
@@ -1,21 +1,19 @@
 package com.teamwss.websoso.data.remote.service
 
 import com.teamwss.websoso.data.remote.request.NicknamePatchRequest
-import com.teamwss.websoso.data.remote.request.NovelPostRequest
+import com.teamwss.websoso.data.remote.response.LoginResponse
 import com.teamwss.websoso.data.remote.response.MyPageUserInfoResponse
-import com.teamwss.websoso.data.remote.response.UserNovelsResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.PATCH
 import retrofit2.http.POST
-import retrofit2.http.Path
 
 interface AuthService {
 
     @POST("users/login")
     suspend fun postLogin(
-    ): UserNovelsResponse
+    ): LoginResponse
 
     @GET("users/info")
     suspend fun getMyPageUserInfo(
