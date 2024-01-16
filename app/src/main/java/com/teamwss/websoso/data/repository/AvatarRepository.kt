a785ff8a47e2b170afa3c044f6ef94e40c864d02
librarywon <librarywon@naver.com>
2024-01-16 21:36:38 +0900

    feat: 리턴 타입 명시

--- app/src/main/java/com/teamwss/websoso/data/repository/AvatarRepository.kt
+++ app/src/main/java/com/teamwss/websoso/data/repository/AvatarRepository.kt
@@ -1,10 +1,12 @@
 package com.teamwss.websoso.data.repository
 
+import com.teamwss.websoso.data.remote.response.AvatarHomeResponse
 import com.teamwss.websoso.data.remote.service.AvatarService
 
 class AvatarRepository(
     private val avatarService: AvatarService
 ) {
 
-    suspend fun getRepresentativeAvatar() = avatarService.getRepresentativeAvatar()
+    suspend fun getRepresentativeAvatar(): AvatarHomeResponse =
+        avatarService.getRepresentativeAvatar()
 }
--- app/src/main/java/com/teamwss/websoso/data/repository/UserNovelsRepository.kt
+++ app/src/main/java/com/teamwss/websoso/data/repository/UserNovelsRepository.kt
@@ -1,12 +1,19 @@
 package com.teamwss.websoso.data.repository
 
+import com.teamwss.websoso.data.remote.response.SosopickNovelHomeResponse
+import com.teamwss.websoso.data.remote.response.UserNovelsLibraryResponse
 import com.teamwss.websoso.data.remote.service.UserNovelService
 
 class UserNovelsRepository(
     private val userNovelService: UserNovelService
 ) {
 
-    suspend fun getUserNovels(readState: String, lastUserNovelId: Long, size: Int, sortType: String) =
+    suspend fun getUserNovels(
+        readState: String,
+        lastUserNovelId: Long,
+        size: Int,
+        sortType: String
+    ): UserNovelsLibraryResponse =
         userNovelService.getUserNovels(
             readStatus = readState,
             lastUserNovelId = lastUserNovelId,
@@ -14,6 +21,7 @@ class UserNovelsRepository(
             sortType = sortType
         )
 
-    suspend fun getSosoPickNovels() = userNovelService.getSosoPickNovels()
+    suspend fun getSosoPickNovels(): SosopickNovelHomeResponse =
+        userNovelService.getSosoPickNovels()
 
 }
