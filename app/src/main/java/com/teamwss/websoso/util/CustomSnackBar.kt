8e32aa90489874f2ce704ef5faa9afbb3348b44b
Son Myeongji <sonmyungj1zz@gmail.com>
2024-01-16 19:11:26 +0900

    feat: 커스텀 스낵바 뷰 구현

---
+++ app/src/main/java/com/teamwss/websoso/util/CustomSnackBar.kt
@@ -0,0 +1,54 @@
+package com.teamwss.websoso.util
+
+import android.graphics.drawable.Drawable
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.FrameLayout
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import com.google.android.material.snackbar.Snackbar
+import com.teamwss.websoso.R
+import com.teamwss.websoso.databinding.CustomSnackBarBinding
+
+class SampleSnackBar(view: View, private val message: String, private val drawable: Drawable) {
+
+    companion object {
+        fun make(view: View, message: String, drawable: Drawable) =
+            SampleSnackBar(view, message, drawable)
+    }
+
+    private val context = view.context
+    private val snackBar = Snackbar.make(view, "", 2000)
+    private val snackBarLayout = snackBar.view as Snackbar.SnackbarLayout
+
+    private val inflater = LayoutInflater.from(context)
+    private val snackBarBinding: CustomSnackBarBinding =
+        DataBindingUtil.inflate(inflater, R.layout.custom_snack_bar, null, false)
+
+    init {
+        initView()
+        initData()
+    }
+
+    private fun initView() {
+        with(snackBarLayout) {
+            val layoutParams = layoutParams as FrameLayout.LayoutParams
+
+            layoutParams.gravity = Gravity.BOTTOM
+            removeAllViews()
+            setPadding(0, 0, 0, 6) // 👈 padding 설정. 위에서 16만큼 떨어져있게.
+            setBackgroundColor(ContextCompat.getColor(context, android.R.color.transparent))
+            addView(snackBarBinding.root, 0)
+        }
+    }
+
+    private fun initData() {
+        snackBarBinding.tvCustomSnackBar.text = message
+        snackBarBinding.ivCustomSnackBar.background = drawable
+    }
+
+    fun show() {
+        snackBar.show()
+    }
+}
