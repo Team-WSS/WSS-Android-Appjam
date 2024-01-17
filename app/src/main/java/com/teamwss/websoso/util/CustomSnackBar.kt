dfc3674ffc4f21d6f3316e0e7ff638a37858a143
Son Myeongji <sonmyungj1zz@gmail.com>
2024-01-17 14:53:45 +0900

    feat: 커스텀 스낵바 수정

--- app/src/main/java/com/teamwss/websoso/util/CustomSnackBar.kt
+++ app/src/main/java/com/teamwss/websoso/util/CustomSnackBar.kt
@@ -1,54 +1,46 @@
-package com.teamwss.websoso.util
-
 import android.graphics.drawable.Drawable
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.content.ContextCompat
-import androidx.databinding.DataBindingUtil
 import com.google.android.material.snackbar.Snackbar
 import com.teamwss.websoso.R
-import com.teamwss.websoso.databinding.CustomSnackBarBinding
 
-class CustomSnackBar(view: View, private val message: String, private val drawable: Drawable) {
-
-    companion object {
-        fun make(view: View, message: String, drawable: Drawable) =
-            CustomSnackBar(view, message, drawable)
-    }
-
-    private val context = view.context
-    private val snackBar = Snackbar.make(view, "", 2000)
-    private val snackBarLayout = snackBar.view as Snackbar.SnackbarLayout
-
-    private val inflater = LayoutInflater.from(context)
-    private val snackBarBinding: CustomSnackBarBinding =
-        DataBindingUtil.inflate(inflater, R.layout.custom_snack_bar, null, false)
+class CustomSnackBar(private val view: View) {
+    private val snackBar: Snackbar = Snackbar.make(view, "", Snackbar.LENGTH_SHORT)
+    private val customView: View
+    private val tvCustomSnackBar: TextView
+    private val ivCustomSnackBar: ImageView
 
     init {
-        initView()
-        initData()
+        val inflater = LayoutInflater.from(view.context)
+        customView = inflater.inflate(R.layout.custom_snack_bar, null)
+        tvCustomSnackBar = customView.findViewById(R.id.tvCustomSnackBar)
+        ivCustomSnackBar = customView.findViewById(R.id.ivCustomSnackBar)
+
+        val snackBarLayout = snackBar.view as Snackbar.SnackbarLayout
+        snackBarLayout.setBackgroundColor(ContextCompat.getColor(view.context, android.R.color.transparent))
+        snackBarLayout.addView(customView, 0)
     }
 
-    private fun initView() {
-        with(snackBarLayout) {
-            val layoutParams = layoutParams as FrameLayout.LayoutParams
-
-            layoutParams.gravity = Gravity.BOTTOM
-            removeAllViews()
-            setPadding(0, 0, 0, 6) // 👈 padding 설정. 위에서 16만큼 떨어져있게.
-            setBackgroundColor(ContextCompat.getColor(context, android.R.color.transparent))
-            addView(snackBarBinding.root, 0)
-        }
+    fun setText(text: String): CustomSnackBar {
+        tvCustomSnackBar.text = text
+        return this
     }
 
-    private fun initData() {
-        snackBarBinding.tvCustomSnackBar.text = message
-        snackBarBinding.ivCustomSnackBar.background = drawable
+    fun setIcon(drawable: Drawable): CustomSnackBar {
+        ivCustomSnackBar.background = drawable
+        return this
     }
 
     fun show() {
         snackBar.show()
+    }
+
+    companion object {
+        fun make(view: View): CustomSnackBar {
+            return CustomSnackBar(view)
+        }
     }
 }
