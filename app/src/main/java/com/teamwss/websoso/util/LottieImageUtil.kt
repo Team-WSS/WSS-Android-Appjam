e06ea9f45e4c889899a279f5c58aa1e84b947a50
librarywon <librarywon@naver.com>
2024-01-17 22:18:39 +0900

    feat: 로티 이미지 추가 및 유틸 수정

--- app/src/main/java/com/teamwss/websoso/util/LottieImageUtil.kt
+++ app/src/main/java/com/teamwss/websoso/util/LottieImageUtil.kt
@@ -3,6 +3,8 @@ package com.teamwss.websoso.util
 import com.teamwss.websoso.R
 
 fun Long.toLottieImage() = when (this) {
-    1L -> R.raw.lottie_home_regressor
-    else -> R.raw.lottie_home_regressor
+    1L -> R.raw.lottie_sosocat_1
+    2L -> R.raw.lottie_villainess_1
+    3L -> R.raw.lottie_regressor_1
+    else -> R.raw.lottie_sosocat_2
 }
