23c483809d81aefee7165c5c77e39b8203ff4807
Son Myeongji <sonmyungj1zz@gmail.com>
2024-01-17 20:00:25 +0900

    refactor: 커스텀 스낵바 클래스에 뷰바인딩 적용

--- app/src/main/java/com/teamwss/websoso/util/CustomSnackBar.kt
+++ app/src/main/java/com/teamwss/websoso/util/CustomSnackBar.kt
@@ -1,36 +1,35 @@
 import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
-import com.teamwss.websoso.R
+import com.teamwss.websoso.databinding.CustomSnackBarBinding
 
 class CustomSnackBar(private val view: View) {
     private val snackBar: Snackbar = Snackbar.make(view, "", Snackbar.LENGTH_SHORT)
-    private val customView: View
-    private val tvCustomSnackBar: TextView
-    private val ivCustomSnackBar: ImageView
+    private val binding: CustomSnackBarBinding
 
     init {
         val inflater = LayoutInflater.from(view.context)
-        customView = inflater.inflate(R.layout.custom_snack_bar, null)
-        tvCustomSnackBar = customView.findViewById(R.id.tvCustomSnackBar)
-        ivCustomSnackBar = customView.findViewById(R.id.ivCustomSnackBar)
+        binding = CustomSnackBarBinding.inflate(inflater)
 
         val snackBarLayout = snackBar.view as Snackbar.SnackbarLayout
-        snackBarLayout.setBackgroundColor(ContextCompat.getColor(view.context, android.R.color.transparent))
-        snackBarLayout.addView(customView, 0)
+        snackBarLayout.setBackgroundColor(
+            ContextCompat.getColor(
+                view.context,
+                android.R.color.transparent
+            )
+        )
+        snackBarLayout.addView(binding.root, 0)
     }
 
     fun setText(text: String): CustomSnackBar {
-        tvCustomSnackBar.text = text
+        binding.tvCustomSnackBar.text = text
         return this
     }
 
     fun setIcon(drawable: Drawable): CustomSnackBar {
-        ivCustomSnackBar.background = drawable
+        binding.ivCustomSnackBar.background = drawable
         return this
     }
 
