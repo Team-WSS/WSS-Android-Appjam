fe9f7ac1634cace43df25b24b72a093c6e37377b
junseo511 <prime54@g.skku.edu>
2024-01-18 18:33:38 +0900

    refactor: 로티 이미지 id 수정 및 2번 이미지 추가

--- app/src/main/java/com/teamwss/websoso/util/LottieImageUtil.kt
+++ app/src/main/java/com/teamwss/websoso/util/LottieImageUtil.kt
@@ -4,7 +4,10 @@ import com.teamwss.websoso.R
 
 fun Long.toLottieImage() = when (this) {
     1L -> R.raw.lottie_sosocat_1
-    2L -> R.raw.lottie_villainess_1
-    3L -> R.raw.lottie_regressor_1
+    2L -> R.raw.lottie_regressor_1
+    3L -> R.raw.lottie_villainess_1
+    101L -> R.raw.lottie_sosocat_2
+    102L -> R.raw.lottie_regressor_2
+    103L -> R.raw.lottie_villainess_2
     else -> R.raw.lottie_sosocat_2
 }
