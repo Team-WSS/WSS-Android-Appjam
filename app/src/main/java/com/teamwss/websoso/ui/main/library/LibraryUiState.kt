8e0b72ef18a693c909b1b622b24d2dbbfa0d79ef
librarywon <librarywon@naver.com>
2024-02-03 15:23:59 +0900

    build: LibraryUiState Uninitialized 상태 제거

--- app/src/main/java/com/teamwss/websoso/ui/main/library/LibraryUiState.kt
+++ app/src/main/java/com/teamwss/websoso/ui/main/library/LibraryUiState.kt
@@ -1,7 +1,6 @@
 package com.teamwss.websoso.ui.main.library
 
 sealed class LibraryUiState {
-    object Uninitialized : LibraryUiState()
     object Loading : LibraryUiState()
     object Resumed : LibraryUiState()
     object Success : LibraryUiState()
